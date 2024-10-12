a591e857269a4fe8935057b0e9d8a97a0f453ff7
AlexandrovVictor-C53 <st131717@spbu.ru>
2024-10-12 22:42:34 +0300

    HW-4 all tasks upload

--- 2024.10.11-HW-4/Project10/Source.cpp
+++ 2024.10.11-HW-4/Project10/Source.cpp
@@ -1,35 +1,44 @@
-#include<cstdio>
+#include <cstdio>
+#include <cmath>
 
 int main(int argc, char* argv[])
 {
-	int x[1001];
-	int i = 0;
-	int n = 0;
 	int a = 0;
 	int b = 0;
+	int n = 0;
 	int c = 0;
 	int d = 0;
-	int y = 0;
 
-	scanf_s("%d", &i);
-	scanf_s("%d", &n);
 	scanf_s("%d", &n);
 	scanf_s("%d", &a);
 	scanf_s("%d", &b);
 	scanf_s("%d", &c);
 	scanf_s("%d", &d);
-	scanf_s("%d", &y);
 
-	
-	for (i = 1;i <= n;i++) {
-		x[i] = i;
+	int m[1000] = { 0 };
+
+	for (int i = 0; i < n; ++i)
+	{
+		m[i] = i + 1;
 	}
-	//...
-	
 
-	for (i = 1;i <= n;i++) {
-		printf("%d", x[i]);
-		printf(" ");
+	for (int i = a - 1, j = b - 1; i < j; ++i, --j)
+	{
+		int f = m[i];
+		m[i] = m[j];
+		m[j] = f;
+	}
+
+	for (int i = c - 1, j = d - 1; i < j; ++i, --j)
+	{
+		int f = m[i];
+		m[i] = m[j];
+		m[j] = f;
+	}
+
+	for (int i = 0; i < n; ++i)
+	{
+		printf(" %d", m[i]);
 	}
 
 	return 0;
